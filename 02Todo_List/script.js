35ef47ac3e760c1318e505b4b533c5859676f49f
mujeeb7020 <sayedmujeeb90@yahoo.com>
2023-11-09 18:33:14 +0530

    complete todo

--- 02Todo_List/script.js
+++ 02Todo_List/script.js
@@ -10,8 +10,22 @@ function addTask(){
         let list=document.createElement("li");
         list.innerHTML=inputBox.value;
         listContainer.appendChild(list);
-        let span=document.creat
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        list.appendChild(span)
     }
 
     inputBox.value="";
 }
+
+
+ listContainer.addEventListener("click",()=>{
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+    }else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    
+    }
+ })
+
+   
